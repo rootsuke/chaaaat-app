1f35ee510b5ba11ca1ab5c034f2c816b232aca65
rootsuke <rootsuke@gmail.com>
2019-04-28 23:37:41 +0900

    delete unused routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
   root to: "rooms#show"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
