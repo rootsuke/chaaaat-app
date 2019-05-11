7f07ff64e606751de056da65dff23f7127535cc4
rootsuke <rootsuke@gmail.com>
2019-05-11 22:21:22 +0900

    add a form to create a new chat room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,30 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_user!, only: %i[show]
+  before_action :authenticate_user!, only: %i[show create]
   def index
     @rooms = Room.all
+    if user_signed_in?
+      @room = Room.new
+    end
+  end
+
+  def create
+    room = Room.create(room_params)
+    if room.save
+      flash[:success] = "room created!"
+      redirect_to root_url
+    else
+      render "rooms/index"
+    end
   end
   
   def show
     @room = Room.find(params[:id])
     @messages = @room.messages.all
   end
+
+  private
+  
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :rooms, only: %i[show]
+  resources :rooms, only: %i[show create]
 end
