7d978de891694da43093e1d95f75ef3bd8501ee6
rootsuke <rootsuke@gmail.com>
2019-05-11 19:08:25 +0900

    create devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :rooms, only: %i[show]
 end
