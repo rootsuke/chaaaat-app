1985a3fae2045fa99f9abc3ae748f9f09ce7f15c
rootsuke <rootsuke@gmail.com>
2019-05-13 22:03:49 +0900

    create relationships of rooms and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages, dependent: :destroy
+  has_many :room_users, dependent: :destroy
+  has_many :rooms, through: :room_users
 
   validates :name, presence: true
 end
