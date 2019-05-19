7b6971ba83d65a5928311d10748f93bd3726b496
rootsuke <rootsuke@gmail.com>
2019-05-19 23:27:04 +0900

    add users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :rooms, param: :room_name, only: %i[show create] do
     get :members, on: :member
   end
+  resources :users, param: :user_name, only: %i(show) do
+    get :search, on: :collection
+  end
 end
