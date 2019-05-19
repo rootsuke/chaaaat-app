8a7826d4230455205dc6f5ec0fd12c563e55dc7e
rootsuke <rootsuke@gmail.com>
2019-05-19 22:54:35 +0900

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # returns full_page_title
+  def full_title(page_title)
+    base_title = "Chat App"
+    return base_title if page_title.empty?
+    "#{page_title} | #{base_title}"
+  end
 end
