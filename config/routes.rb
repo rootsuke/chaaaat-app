05727879208dffa8f7e97fc97ea6b7b899d392a8
rootsuke <rootsuke@gmail.com>
2019-05-20 16:55:13 +0900

    add rooms/index link in the header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :rooms, param: :room_name, only: %i[show create] do
+  resources :rooms, param: :room_name, only: %i[index show create] do
     get :members, on: :member
   end
   resources :users, param: :user_id, only: %i(show) do
