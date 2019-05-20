52dd490d7230d6d4c380248dc06efe0f7ecf1e42
rootsuke <rootsuke@gmail.com>
2019-05-20 22:38:11 +0900

    add user's friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(user_id: params[:user_id])
   end
+
+  def friends
+    @user = User.find_by(user_id: params[:user_id])
+    @users = @user.friends
+  end
 end
