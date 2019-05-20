adf825b689038336c2e68c7bedad56a0108dfbe6
rootsuke <rootsuke@gmail.com>
2019-05-20 16:35:50 +0900

    create user/show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find_by(user_id: params[:user_id])
+  end
 end
