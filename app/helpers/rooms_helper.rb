6ed055590e75310f6ad19d72e892115a4d3a10b2
rootsuke <rootsuke@gmail.com>
2019-05-22 22:39:59 +0900

    create room partial

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,10 @@
 module RoomsHelper
+  def render_latest_message(room)
+    msg = room.messages.last
+    if msg
+      message = content_tag(:h4, msg.content)
+      message += content_tag(:span, l(msg.created_at))
+      message
+    end
+  end
 end
