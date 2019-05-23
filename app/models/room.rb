e9e3d39e67a6049fbf39467120b4891a359f9af0
rootsuke <rootsuke@gmail.com>
2019-05-23 23:36:39 +0900

    create room_icon uploader

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,15 @@ class Room < ApplicationRecord
   has_many :users, through: :room_users
 
   validates :name, presence: true
+  validate  :icon_size
+
+  mount_uploader :icon, RoomIconUploader
+
+  private
+  
+    def icon_size
+      if icon.size > 5.megabytes
+        errors.add(:icon, "should be less than 5MB")
+      end
+    end
 end
