dd860e05672bd8504a0c5f4d0226660e7e076d1e
rootsuke <rootsuke@gmail.com>
2019-05-23 23:23:08 +0900

    change default user_icon

--- app/uploaders/user_icon_uploader.rb
+++ app/uploaders/user_icon_uploader.rb
@@ -25,7 +25,7 @@ class UserIconUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "default.png"
+    "user_default.png"
   end
 
   # Process files as they are uploaded:
