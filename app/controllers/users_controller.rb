51c4e3261a00dddd816e5dc7387a99e3864b51bc
rootsuke <rootsuke@gmail.com>
2019-05-24 15:40:14 +0900

    add searching user formUser.find_by(user_id: current_user.user_id).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,28 @@ class UsersController < ApplicationController
   end
 
   def friends
-    @user = User.find_by(user_id: params[:user_id])
-    @users = @user.friends.order(name: :asc)
-    @friend_requests = FriendRequest.new_request_to(current_user)
+    set_friends_and_requests
   end
+
+  def search
+    if params[:search].blank?
+      flash[:danger] = "Please enter the user_id."
+      redirect_to friends_user_path(current_user.user_id)
+    elsif User.exists?(user_id: params[:search])
+      redirect_to user_path(params[:search])
+    else
+      flash.now[:danger] = "Cannot find a user."
+      @search_value = params[:search]
+      set_friends_and_requests
+      render "friends"
+    end
+  end
+
+  private
+
+    def set_friends_and_requests
+      user = User.find_by(user_id: current_user.user_id)
+      @users = user.friends.order(name: :asc)
+      @friend_requests = FriendRequest.new_request_to(current_user)
+    end
 end
